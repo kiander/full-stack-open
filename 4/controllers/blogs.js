89c2977a9e6d6a3513b916517ef858a7197f7b26
Otto Kiander <otto.kiander@tuni.fi>
2022-02-11 15:40:42 +0200

    token excercises

--- 4/controllers/blogs.js
+++ 4/controllers/blogs.js
@@ -1,34 +1,56 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-blogsRouter.get('/', async (request, response) => { 
+const getTokenFrom = request =>
+{
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer '))
+  {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response) =>
+{
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.get('/:id', async (request, response) => {
+blogsRouter.get('/:id', async (request, response) =>
+{
   const blog = await Blog.findById(request.params.id)
-  if (blog) {
+  if (blog)
+  {
     response.json(blog.toJSON())
-  } else {
+  } else
+  {
     response.status(404).end()
   }
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response) =>
+{
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id)
+  {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id  
+    user: user._id
   })
 
   const savedBlog = await blog.save()
@@ -38,9 +60,10 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON())
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+blogsRouter.delete('/:id', async (request, response) =>
+{
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
